d0e68b10c1f5c68ae65f38bd95e08396f5a8e2af
Tempest <krukovtempest@gmail.com>
2024-01-03 10:50:48 -0600

    Refactored BasePawn to BaseChararcter
    +BP is named BaseChar
    +Everything the pawn could do before is now in BaseChar/BaseCharacter

---
+++ Source/Spellblade/BaseCharacter.cpp
@@ -0,0 +1,167 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCharacter.h"
+#include "SpellBase.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ABaseCharacter::ABaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// CapsuleCom = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule collider"));
+	// RootComponent = CapsuleCom;
+
+	Spring = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	Spring->SetupAttachment(RootComponent);
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(Spring);
+	//
+	// BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("base mesh"));
+	// BaseMesh->SetupAttachment(CapsuleCom);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn"));
+	ProjectileSpawnPoint->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController)
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(CharacterMappingContext, 0);
+		}
+	}
+	
+}
+
+// Called every frame
+void ABaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// binding inputs to functions
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Look);
+		EnhancedInputComponent->BindAction(SpellementSelectAction, ETriggerEvent::Triggered, this, &ABaseCharacter::SelectSpellement);
+		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Fire);
+	}
+
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ABaseCharacter::Turn);
+
+}
+
+void ABaseCharacter::Move(const FInputActionValue& Value)
+{
+	const float DirectionValue = Value.Get<float>();
+
+	if (PlayerController && DirectionValue != 0.f)
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, DirectionValue);
+	}
+}
+
+// look around using mouse
+void ABaseCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	if (GetController())
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+}
+
+void ABaseCharacter::Turn(float Value) {
+
+	AddControllerYawInput(Value);
+
+	// FRotator DeltaRotation = FRotator::ZeroRotator;
+	// DeltaRotation.Yaw = Value * UGameplayStatics::GetWorldDeltaSeconds(this) * TurnRate;
+
+	// AddActorLocalRotation(DeltaRotation, true);
+}
+
+void ABaseCharacter::Fire()
+{
+	if (IsFiring == false)
+	{
+		IsFiring = true;
+		FVector ProjectileSpawnPointLocation = ProjectileSpawnPoint->GetComponentLocation();
+		FRotator ProjectileSpawnPointRotation = ProjectileSpawnPoint->GetComponentRotation();
+
+		// DrawDebugSphere(
+		// 	GetWorld(),
+		// 	ProjectileSpawnPointLocation,
+		// 	25.f,
+		// 	12,
+		// 	FColor::Red,
+		// 	false,
+		// 	3.f
+		// );
+		
+		// ProjectileClass here is whatever Blueprint is set as the projectile on the BP of the character
+		if (ProjectileClass)
+		{
+			ASpellBase* Projectile = GetWorld()->SpawnActor<ASpellBase>(ProjectileClass, ProjectileSpawnPointLocation, ProjectileSpawnPointRotation);
+			Projectile->SetOwner(this);
+			Projectile->SetSpellementType(CurrentlySelectedElement);
+		}
+		else {UE_LOG(LogTemp, Error, TEXT("Projectile Class not set!"));}
+	}
+	IsFiring = false;
+}
+
+void ABaseCharacter::SelectSpellement(const FInputActionValue& Value)
+{
+	const float result = Value.Get<float>();
+	if (result == 1)
+	{
+		CurrentlySelectedElement = ESpellementType::Wind;
+	}
+	else if (result == -1)
+	{
+		CurrentlySelectedElement = ESpellementType::Water;
+	}
+	else if (result == 2)
+	{
+		UE_LOG(LogTemp, Display, TEXT("SELECTED FIRE"));
+		CurrentlySelectedElement = ESpellementType::Fire;
+	}
+	else if (result == 3)
+	{
+		UE_LOG(LogTemp, Display, TEXT("SELECTED EARTH"));
+		CurrentlySelectedElement = ESpellementType::Earth;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("BasePawn.cpp: Could not find correct Spellement Type"));
+	}
+}
+
